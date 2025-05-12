02948c8e261678dad6ec80217d679c7c018149c4
odnath <od.nathmnair1@gmail.com>
2025-05-12 19:21:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-function firstWord(s) {
-  // your code here
+function firstWord(str) {
+  // Trim leading whitespace first to handle cases like ' Hello'
+  str = str.trim();
+
+  // Find the index of the first space
+  let spaceIndex = str.indexOf(' ');
+
+  // If there's no space, return the entire string
+  if (spaceIndex === -1) {
+    return str;
+  }
+
+  // Return the substring up to the first space
+  return str.slice(0, spaceIndex);
 }
 
-// Do not change the code below
-
-const s = prompt("Enter String:");
-alert(firstWord(s));
